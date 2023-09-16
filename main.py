273f8067d3d2f8a571ccf738dbe0efd0c02f1035
Vanik <40670451+VanikSarajyan@users.noreply.github.com>
2023-09-16 17:28:15 +0000

    integrated wiki

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import uvicorn
 from fastapi import FastAPI
+
+
 from lib.logic import wiki
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/wiki")
+async def wiki():
+    return {"wiki": wiki()}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
